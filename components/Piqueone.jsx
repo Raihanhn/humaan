12170e483d162c0d5e5ebc6b7088ef914e221862
Mohammad Raihan <108555328+Raihanhn@users.noreply.github.com>
2025-10-02 13:14:19 +0600

    video updated again

--- components/Piqueone.jsx
+++ components/Piqueone.jsx
@@ -4,26 +4,26 @@ import Image from "next/image";
 
 export default function Piqueone() {
   return (
-    <div className="relative bg-[#262262] w-full h-[120vh] sm:h-[100vh] xs:h-[80vh] rounded-3xl overflow-hidden pt-8 ">
+    <div className="relative bg-[#262262] w-full h-[120vh] sm:h-[100vh] xs:h-[80vh] rounded-3xl overflow-hidden pt-8">
       {/* Background Image */}
       <Image
-        src="/images/hq.jpg" 
+        src="/images/hq.jpg"
         alt="Pique"
         fill
         className="object-cover"
-        priority 
+        priority
       />
 
-      {/* Centered Video */}
+      {/* Centered YouTube Video */}
       <div className="absolute inset-0 flex justify-center items-center px-4">
         <div className="w-full sm:w-[90%] md:w-[70%] lg:w-[70%] aspect-video rounded-3xl overflow-hidden shadow-lg">
-          <video
-            src="https://player.vimeo.com/progressive_redirect/playback/894026344/rendition/720p/file.mp4?loc=external&log_user=0&signature=defd0324929609083c0f37054720fef27100e9240b3a6eed5566f0d10cbf414a"
-            autoPlay
-            loop
-            playsInline
-            muted
-            className="w-full h-full object-cover"
+          <iframe
+            className="w-full h-full"
+            src="https://www.youtube.com/embed/JoQjviMKcUo?autoplay=1&mute=1&controls=0&loop=1&playlist=JoQjviMKcUo&modestbranding=1&rel=0&start=2"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
           />
         </div>
       </div>
